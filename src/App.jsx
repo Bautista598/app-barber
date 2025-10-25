c2c13efdcb512468d6273ba036f8d209205aa799
Bau_ <basach1582@gmail.com>
2025-10-25 09:59:19 -0300

    Corregí unos problemas que tenia con la importación de "react-router-dom" y lo corregí por "reacr-router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
-// import AgendarCita from './pages/AgendarCita'
-// import VerCitas from './pages/VerCitas'
 
 function App() {
 	return (
