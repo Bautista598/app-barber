bd782137303a61275dd25e93531c32a4d82e1a3f
Bau_ <basach1582@gmail.com>
2025-10-25 10:16:41 -0300

    Agregue la ruta de "/ver-citas"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
+import SeeQuotes from './pages/SeeQuotes'
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Layout>
 				<Routes>
 					<Route path="/" index element={<Dashboard />} />
+					<Route path="/ver-citas" index element={<SeeQuotes />} />
 				</Routes>
 			</Layout>
 		</BrowserRouter>
