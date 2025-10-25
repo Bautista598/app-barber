4795ce95dcb2ca576a90da0cc69a9022e89c80fc
Bau_ <basach1582@gmail.com>
2025-10-25 10:01:04 -0300

    Cree los elementos y los estilice, importe los componentes barberTable y statsCards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import StatsCards from '../components/StatsCards'
+import BarberTable from '../components/BarberTable'
+
 function Dashboard() {
-	return <h1>Helloword</h1>
+	return (
+		<>
+			<div className="min-h-screen ">
+				<main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+					<div className="mb-8">
+						<StatsCards />
+					</div>
+
+					<div className="bg-white rounded-xl shadow-sm border border-slate-200 p-6">
+						<div className="mb-6">
+							<h2 className="text-2xl font-bold text-slate-900">Barberos</h2>
+							<p className="text-slate-600 mt-1">
+								Gestiona y consulta el rendimiento de todos los barberos
+							</p>
+						</div>
+						<BarberTable />
+					</div>
+				</main>
+			</div>
+		</>
+	)
 }
 
 export default Dashboard
