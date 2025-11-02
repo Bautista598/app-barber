0daac05d31c9f23df47a6f2a1cdc34d96ddbaa40
Bau_ <basach1582@gmail.com>
2025-11-02 11:56:15 -0300

    creación de la pagina de agendar cita

---
+++ src/pages/AddAppointment.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function AddAppointment() {
+	const [loading, setLoading] = useState(true)
+
+	if (!loading) {
+		return (
+			<div className="flex items-center justify-center h-96">
+				<div className="animate-spin rounded-full h-12 w-12 border-b-2 border-amber-400"></div>
+			</div>
+		)
+	}
+
+	return (
+		<h1 className="text-xl text-white">
+			Aquí irán los componente de los 4 pasos y la lógica para agendar la cita
+		</h1>
+	)
+}
+
+export default AddAppointment
