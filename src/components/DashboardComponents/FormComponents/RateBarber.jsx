a5ec34e70f805159ef65afedd102b56cf162dd46
Bau_ <basach1582@gmail.com>
2025-11-07 20:42:12 -0300

    Finalización de los estilos del componente para seleccionar horario

--- src/components/DashboardComponents/FormComponents/RateBarber.jsx
+++ src/components/DashboardComponents/FormComponents/RateBarber.jsx
@@ -5,20 +5,38 @@ function RateBarber() {
 		<div>
 			<label
 				htmlFor="puntuacion"
-				className="block text-sm font-medium text-gray-700"
+				className="block tracking-widest text-sm font-medium text-white"
 			>
 				Puntuación (Opcional)
 			</label>
-			<input
-				type="number"
-				id="puntuacion"
-				name="puntuacion"
-				min="0"
-				max="5"
-				step="0.1"
-				placeholder="0.0 a 5.0"
-				className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-			/>
+			<div
+				className="mt-1 
+      p-[3px]  
+      rounded-xl 
+      bg-linear-to-br from-blue-400 to-blue-600  
+      shadow-lg"
+			>
+				<input
+					type="number"
+					id="puntuacion"
+					name="puntuacion"
+					min="0"
+					max="5"
+					step="0.1"
+					placeholder="0.0 a 5.0"
+					className=" block 
+        w-full 
+        p-2 
+        bg-linear-to-br from-slate-900 via-slate-800 to-slate-900
+        rounded-[10px]
+        shadow-inner 
+        text-gray-400
+				transition-colors
+				duration-200
+        focus:outline-none 
+				focus:text-white"
+				/>
+			</div>
 		</div>
 	)
 }
