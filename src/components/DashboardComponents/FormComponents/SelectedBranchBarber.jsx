930e698f5971502c4c261e075418e7e7a46cf9bd
Bau_ <basach1582@gmail.com>
2025-11-07 20:42:42 -0300

    lve mejora en los estilos para seleccionar una sucursal

--- src/components/DashboardComponents/FormComponents/SelectedBranchBarber.jsx
+++ src/components/DashboardComponents/FormComponents/SelectedBranchBarber.jsx
@@ -1,13 +1,23 @@
 function SelectedBranchBarber() {
 	return (
-		<div>
+		<div className="flex flex-col">
 			<label
 				htmlFor="sucursal"
-				className="block text-sm font-medium text-gray-700"
+				className="tracking-widest text-sm font-medium text-white"
 			>
 				Sucursal <span className="text-red-500">*</span>
 			</label>
-			<select id="sucursal" name="sucursal" required>
+			<select
+				id="sucursal"
+				name="sucursal"
+				required
+				className="mt-1 
+				outline-0
+      rounded-xl 
+			h-9
+      bg-linear-to-br from-blue-400 to-blue-600  
+      shadow-lg focus:border-0 text-white"
+			>
 				<option value="">Selecciona una sucursal</option>
 				<option value="principal">Principal</option>
 				<option value="norte">Norte</option>
