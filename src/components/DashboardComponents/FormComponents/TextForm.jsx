a14209866df9bf9bab6e4b39c73415f005e665e4
Bau_ <basach1582@gmail.com>
2025-11-07 15:36:32 -0300

    estilos focus del input text

--- src/components/DashboardComponents/FormComponents/TextForm.jsx
+++ src/components/DashboardComponents/FormComponents/TextForm.jsx
@@ -32,8 +32,9 @@ function TextForm({ placeholder, label }) {
         bg-linear-to-br from-slate-900 via-slate-800 to-slate-900
         rounded-[10px]
         shadow-inner 
-        text-white 
+        text-gray-300	
         focus:outline-none 
+				focus:text-white
       "
 				/>
 			</div>
