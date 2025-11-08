b3ae4a6e9cc903fd65c1c912fcfee097e0567879
Bau_ <basach1582@gmail.com>
2025-11-08 11:35:12 -0300

    creación de condicionales en los estilos para mostrar un border con gradiente azul por defecto y en caso de error un rojo y si todo esta bien un verde

--- src/components/DashboardComponents/FormComponents/TextForm.jsx
+++ src/components/DashboardComponents/FormComponents/TextForm.jsx
@@ -1,4 +1,35 @@
 function TextForm({ placeholder, label, name, handleChange, error, value }) {
+	const isDirty = !!value // True si el campo tiene algún valor
+	const hasError = !!error
+
+	const stateValidateStyle = `mt-1 
+    p-[3px] 
+    rounded-xl 
+    shadow-lg
+    transition-all duration-300 ease-in-out
+    ${
+			hasError // 1. Prioridad: ¿Hay error? -> ROJO
+				? 'bg-gradient-to-br from-red-500 to-red-700'
+				: isDirty // 2. Siguiente prioridad: ¿Hay valor (y no hay error)? -> VERDE
+				? 'bg-gradient-to-br from-green-500 to-green-700'
+				: 'bg-gradient-to-br from-blue-400 to-blue-600' // 3. Por defecto (vacío y sin error): AZUL
+		}
+			`
+
+	const inputClasses = `
+    block 
+    w-full 
+    p-2 
+    bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900
+    rounded-[10px]
+    shadow-inner 
+    transition-colors
+    duration-200
+    focus:outline-none 
+    
+    ${hasError ? 'text-red-400' : 'text-gray-400 focus:text-white'}
+  `
+
 	return (
 		<div>
 			<label
@@ -8,15 +39,7 @@ function TextForm({ placeholder, label, name, handleChange, error, value }) {
 				{label}
 			</label>
 
-			<div
-				className="
-      mt-1 
-      p-[3px]  
-      rounded-xl 
-      bg-linear-to-br from-blue-400 to-blue-600  
-      shadow-lg
-    "
-			>
+			<div className={stateValidateStyle}>
 				<input
 					type="text"
 					id={label}
@@ -25,19 +48,7 @@ function TextForm({ placeholder, label, name, handleChange, error, value }) {
 					value={value}
 					placeholder={placeholder}
 					required
-					className="
-        block 
-        w-full 
-        p-2 
-        bg-linear-to-br from-slate-900 via-slate-800 to-slate-900
-        rounded-[10px]
-        shadow-inner 
-        text-gray-400
-				transition-colors
-				duration-200
-        focus:outline-none 
-				focus:text-white
-      "
+					className={inputClasses}
 				/>
 			</div>
 			{error && <p className="text-xs text-red-500 mt-1">{error}</p>}
