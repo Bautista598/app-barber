86f9a490ff15fca23b3c92c8b95a36207516f5dc
Bau_ <basach1582@gmail.com>
2025-11-21 18:02:55 -0300

    creación de la función de getDataTotals para la suma de todos los clientes y ganancias

--- src/components/StatsCards.jsx
+++ src/components/StatsCards.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 // Iconos
 import { DollarSign, Users } from 'lucide-react'
 
@@ -6,14 +8,61 @@ import StatCard from './StatCard'
 import Clock from './Clock'
 import Weather from './Weather'
 
-function StatsCards({ earnTod, custTod }) {
+const getDataTotals = (dataObj) => {
+	// dataObj DEBE ser el array de barberos
+	const totals = dataObj.reduce(
+		(accumulator, currentItem) => {
+			// Suma la ganancia del barbero actual al acumulador
+			accumulator.earningsTotal += +currentItem.earnings_today
+
+			// Suma los clientes del barbero actual al acumulador
+			accumulator.customersTotal += +currentItem.customers_today
+
+			// Retorna el acumulador actualizado para la siguiente iteración
+			return accumulator
+		},
+		{ earningsTotal: 0, customersTotal: 0 } // Estado inicial del acumulador
+	)
+
+	return totals // Retorna el objeto final con las sumas
+}
+
+function StatsCards() {
+	const [totals, setTotals] = useState({
+		earningsTotal: 0,
+		customersTotal: 0,
+	})
+
+	useEffect(() => {
+		const getValueCard = async () => {
+			const URL = '/data_test/data_barber.json'
+			try {
+				const res = await fetch(URL)
+				if (!res.ok) {
+					throw new Error(`HTTP Error: ${res.status}`)
+				}
+
+				const dataNum = await res.json()
+
+				const calculatedTotals = getDataTotals(dataNum)
+
+				setTotals(calculatedTotals)
+			} catch (error) {
+				console.error('Error al cargar datos:', error)
+				setTotals({ earningsTotal: 'Error', customersTotal: 'Error' })
+			}
+		}
+
+		getValueCard()
+	}, [])
+
 	return (
 		<div className="grid gap-6 sm:grid-cols-4">
 			<StatCard
 				icon={DollarSign}
 				gradient="green"
 				label="Ganancia Total"
-				value={earnTod.toFixed(2)}
+				value={totals.earningsTotal}
 				isMoney
 			/>
 
@@ -21,7 +70,7 @@ function StatsCards({ earnTod, custTod }) {
 				icon={Users}
 				gradient="blue"
 				label="Clientes Atendidos"
-				value={custTod}
+				value={totals.customersTotal}
 			/>
 			<Clock />
 			<Weather />
